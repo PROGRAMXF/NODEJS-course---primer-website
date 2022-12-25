dc1c9fb78ea7720fa3c04f8869d9947345ef8ce5
Fernando <fh1273509@gmail.com>
2022-12-25 18:42:39 -0300

    website con express.js

---
+++ src/index.js
@@ -0,0 +1,15 @@
+
+import express, { request, response } from 'express'
+
+const app = express()
+
+app.get('/',(request, response) =>{
+    response.send('<h1>BIENVENIDO</h1>')
+})
+app.get('/about',(request, response) =>{
+    response.send('<h1>about</h1>')
+})
+
+app.listen(3000)
+console.log("server on port", 3000)
+
